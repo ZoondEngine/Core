03cce9e7939c9654dc4da16e753eb62cbd9b1450
ZoondEngine <mango.kamchatka.1997@gmail.com>
2018-04-28 07:49:50 +0300

    Misc fixes
    Misc fixes

--- crappy.cpp
+++ crappy.cpp
@@ -1,5 +1,4 @@
 #include "Utils.h"
-#include "overlay.h"
 #include "Headers.h"
 #include "SDK.hpp"
 
@@ -30,6 +29,21 @@ namespace Ptr
 	uint32_t actorCount = 0;
 	uint64_t localPlayerActor = 0;
 	uint64_t gNames = 0;
+}
+
+namespace SDK 
+{
+	Classes::UWorld* GameInstance;
+	Classes::UWorld* GameState; //Zone partition
+	Classes::UGameInstance* UGameInstance;
+	Classes::ULocalPlayer* ViewportClient;
+	Classes::UPlayer* PlayerController;
+	Classes::APlayerController* PCameraManager;
+	Classes::APlayerCameraManager* CameraCache;
+	Classes::UGameViewportClient* PWorld;
+	Classes::UWorld* PersistentLevel;
+	Classes::ULevel* ActorArray;
+	Classes::ADroppedItemGroup* DroppedItemGroup;
 }
 
 namespace PlayerHooks
@@ -384,7 +398,7 @@ void ESP()
 	UpdateAddresses();
 	wchar_t entityname[64] = { NULL };
 	DWORD_PTR enlist = GetEntityList();
-	int entitycount = mem->read<int>(mem->read<DWORD_PTR>(global::pUWorld + 0x30, 0x8) + 0xA8, 0x4);
+	int entitycount = mem->read<int>(mem->read<DWORD_PTR>(Ptr::uWorld + 0x30, 0x8) + 0xA8, 0x4);
 	float health = 0.f;
 	float distance = 0.f;
 	DWORD color = 0;
@@ -402,13 +416,22 @@ void ESP()
 		int id = mem->read<int>(entity + 0x18, 0x4);
 		if (PlayerHooks::ShowPlayer && (id ==  ActorIds[0] || id == ActorIds[1] || id == ActorIds[2] || id == ActorIds[3]))
 		{
+			Vector3 pos = GetActorPos(entity);
+			Vector3 spos = WorldToScreen(pos, global::cameracache);
+
 			//ShowPlayer();
+			if (PlayerHooks::ShowPlayerDistance)
+			{
+				/*
+				color = D3DCOLOR_ARGB(0, 255, 255, 0);
+				Classes::AActor* enemyActor = reinterpret_cast<Classes::AActor*>(id);
+				DrawString((int)spos.x + 5, (int)spos.y, color, pFont, "%0.1f", ReturnDistance(enemyActor));
+				*/
+			}
 
 			health = GetActorHealth(entity);
 			if (health > 0.f)
 			{
-				Vector3 pos = GetActorPos(entity);
-				Vector3 spos = WorldToScreen(pos, global::cameracache);
 				distance = local.Distance(pos) / 100.f;
 				if (distance > 400.f)
 					continue;
@@ -566,11 +589,333 @@ void SetupWindow()
 	initD3D(hWnd);
 }
 
+#pragma region Overlay
+#define HJWND_PROGRAM L"notepad.exe"
+#define MAX_CLASSNAME 255
+#define MAX_WNDNAME 255
+#define TRANSPARENCY_COLOR RGB(0, 254, 0)
+
+using namespace std;
+
+// Structs
+struct WindowsFinderParams {
+	DWORD pidOwner = NULL;
+	wstring wndClassName = L"";
+	wstring wndName = L"";
+	RECT pos = { 0, 0, 0, 0 };
+	POINT res = { 0, 0 };
+	float percentAllScreens = 0.0f;
+	float percentMainScreen = 0.0f;
+	DWORD style = NULL;
+	DWORD styleEx = NULL;
+	bool satisfyAllCriteria = false;
+	vector<HWND> hwnds;
+};
+
+namespace Overlay
+{
+	HDC HdcObject;
+	HDC HdcBackBuffer;
+	BITMAPINFO BackBufferBmpInfo;
+	HBITMAP HbmBackBuffer;
+	POINT WindowPoint;
+	HFONT HFont;
+}
+
+// Prototypes
+vector<HWND> WindowsFinder(WindowsFinderParams params);
+BOOL CALLBACK EnumWindowsCallback(HWND hwnd, LPARAM lParam);
+HWND HiJackNotepadWindow();
+vector<DWORD> GetPIDs(wstring targetProcessName);
+
+void SetDCText(wstring text, int l, int r, int t, int b)
+{
+	RECT textPos;
+
+	textPos.left = Overlay::WindowPoint.x / l;
+	textPos.right = Overlay::WindowPoint.x / r;
+	textPos.top = 100;
+	textPos.bottom = 100;
+	SetTextColor(Overlay::HdcBackBuffer, RGB(232, 205, 0));
+	DeleteObject(SelectObject(Overlay::HdcBackBuffer, Overlay::HFont));
+	DrawTextExW(Overlay::HdcBackBuffer, (wchar_t*)text.c_str(), text.size(), &textPos, DT_CENTER | DT_NOCLIP | DT_NOPREFIX, NULL);
+}
+
+void InitializeOverlay() {
+	HWND hwnd = HiJackNotepadWindow();
+	if (!hwnd) {
+		cout << "Window HiJacking failed (use debugger to investigate why)" << endl;
+		return;
+	}
+	FreeConsole(); // We won't be needing that anymore
+
+	Overlay::HdcObject = GetDC(hwnd);
+
+	// Getting settings of back buffer bitmap
+	DEVMODE devMode;
+	devMode.dmSize = sizeof(devMode);
+	EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &devMode);
+	Overlay::BackBufferBmpInfo;
+	Overlay::BackBufferBmpInfo = { 0 };
+	Overlay::BackBufferBmpInfo.bmiHeader.biBitCount = devMode.dmBitsPerPel;
+	Overlay::BackBufferBmpInfo.bmiHeader.biHeight = GetSystemMetrics(SM_CYSCREEN);
+	Overlay::BackBufferBmpInfo.bmiHeader.biWidth = GetSystemMetrics(SM_CXSCREEN);
+	Overlay::BackBufferBmpInfo.bmiHeader.biPlanes = 1;
+	Overlay::BackBufferBmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+
+	void* backBufferPixels = nullptr;
+	Overlay::WindowPoint = { GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN) };
+	HBRUSH bgTransparencyColor = CreateSolidBrush(TRANSPARENCY_COLOR);
+	Overlay::HFont = CreateFontA(50, 0, 0, 0, FW_DONTCARE, 0, 0, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, NONANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Consolas");
+	HBRUSH hbrRed = CreateSolidBrush(RGB(255, 0, 0));
+	HPEN hPen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
+
+	//i = (i > Overlay::WindowPoint.x) ? 0 : ++i; // To simulate movement
+
+	// Frame preparation
+	Overlay::HdcBackBuffer = CreateCompatibleDC(Overlay::HdcObject); // Create back buffer
+	SetBkMode(Overlay::HdcBackBuffer, TRANSPARENT); // Text background transparency
+	Overlay::HbmBackBuffer = CreateDIBSection(Overlay::HdcBackBuffer, (BITMAPINFO *)&Overlay::BackBufferBmpInfo, DIB_RGB_COLORS, (void **)&backBufferPixels, NULL, 0); // Create back buffer bitmap
+	DeleteObject(SelectObject(Overlay::HdcBackBuffer, Overlay::HbmBackBuffer));
+	DeleteObject(SelectObject(Overlay::HdcBackBuffer, bgTransparencyColor)); // To set background in transparent color key
+	DeleteObject(SelectObject(Overlay::HdcBackBuffer, hPen));
+	
+   // *  Making transparency
+
+	// Draw calls
+	wstring text = L"M416\n[43M]";
+	RECT textPos;
+
+	//SelectObject(hdcBackBuffer, hbrRed);
+	//Rectangle(hdcBackBuffer, i - 40, res.y / 2 - 180, i + 40, res.y / 2 - 4);
+	SelectObject(Overlay::HdcBackBuffer, bgTransparencyColor);
+	Rectangle(Overlay::HdcBackBuffer, 39, Overlay::WindowPoint.y / 2 - 179, 39, Overlay::WindowPoint.y / 2 - 5);
+
+	textPos.left = Overlay::WindowPoint.x / 8;
+	textPos.right = Overlay::WindowPoint.x / 2;
+	textPos.top = Overlay::WindowPoint.y / 2;
+	textPos.bottom = Overlay::WindowPoint.y / 2;
+	SetTextColor(Overlay::HdcBackBuffer, RGB(255, 0, 0));
+	DrawTextExW(Overlay::HdcBackBuffer, (wchar_t*)text.c_str(), text.size(), &textPos, DT_CENTER | DT_NOCLIP | DT_NOPREFIX, NULL);
+
+	// Frame presentation
+	BitBlt(Overlay::HdcObject, 0, 0, Overlay::WindowPoint.x, Overlay::WindowPoint.y, Overlay::HdcBackBuffer, 0, 0, SRCCOPY);
+
+}
+
+void MakeTrasparency()
+{
+	while (true)
+	{
+		Rectangle(Overlay::HdcBackBuffer, 0, 0, Overlay::WindowPoint.x, Overlay::WindowPoint.y);
+	}
+}
+
+void ClearDCBuffers()
+{
+	DeleteDC(Overlay::HdcBackBuffer); // Delete back buffer device context
+	DeleteObject(Overlay::HbmBackBuffer); // Delete back buffer bitmap
+}
+
+HWND HiJackNotepadWindow() {
+	HWND hwnd = NULL;
+
+	// Remove previous windows
+	vector<DWORD> existingNotepads = GetPIDs(HJWND_PROGRAM);
+	if (!existingNotepads.empty()) {
+		for (int i(0); i < existingNotepads.size(); ++i) {
+			// Terminating processes
+			HANDLE hOldProcess = OpenProcess(PROCESS_TERMINATE, FALSE, existingNotepads[i]);
+			TerminateProcess(hOldProcess, 0);
+			CloseHandle(hOldProcess);
+		}
+	}
+
+	system("start notepad"); // Start notepad, and not as child process, so easy :)
+
+							 /*
+							 // Starts notepad (as child process, unsafe)
+							 wchar_t wWinDir[MAX_PATH] = L"";
+							 GetWindowsDirectory(wWinDir, MAX_PATH);
+							 wstring winDir = wWinDir;
+							 wstring notepadExe = winDir + L"\\notepad.exe";
+							 STARTUPINFO si;
+							 SecureZeroMemory(&si, sizeof(si));
+							 si.cb = sizeof(si);
+							 PROCESS_INFORMATION pi;
+							 SecureZeroMemory(&pi, sizeof(pi));
+							 if (!CreateProcess(notepadExe.c_str(), 0, 0, 0, 0, 0, 0, 0, &si, &pi))
+							 return false;
+							 */
+
+							 // Finding notepad's window (we could just use FindWindow but then it would be OS language dependent)
+	vector<DWORD> notepads = GetPIDs(HJWND_PROGRAM);
+	if (notepads.empty() || notepads.size() > 1) // Should check if more than one to be more strict
+		return hwnd;
+	WindowsFinderParams params;
+	params.pidOwner = notepads[0];
+	params.style = WS_VISIBLE;
+	params.satisfyAllCriteria = true;
+	vector<HWND> hwnds;
+	int attempt = 0; // The process takes a bit of time to initialise and spawn the window, will try during 5 sec before time out
+	while (hwnd == NULL || attempt > 50) {
+		Sleep(100);
+		hwnds = WindowsFinder(params);
+		if (hwnds.size() > 1)
+			return hwnd;
+		hwnd = hwnds[0];
+		++attempt;
+	}
+	if (!hwnd)
+		return hwnd;
+
+	// Making the window usable for overlay puposes
+	SetMenu(hwnd, NULL);
+	SetWindowLongPtr(hwnd, GWL_STYLE, WS_VISIBLE);
+	SetWindowLongPtr(hwnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT); // WS_EX_NOACTIVATE  and WS_EX_TOOLWINDOW removes it from taskbar
+	SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), SWP_SHOWWINDOW);
+
+	// Transparency
+	/* I'm using the shitty color key system for transparency here, it's not performant so for actual cheating prefer DwmExtendFrameIntoClientArea.
+	However this method requires you to fix your alpha channel for images generated with GDI (the alpha channel is left at 0 = transparent) */
+	SetLayeredWindowAttributes(hwnd, TRANSPARENCY_COLOR, 0, LWA_COLORKEY);
+
+	return hwnd;
+}
+
+vector<DWORD> GetPIDs(wstring targetProcessName) {
+	vector<DWORD> pids;
+	if (targetProcessName == L"")
+		return pids;
+	HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32W entry;
+	entry.dwSize = sizeof entry;
+	if (!Process32FirstW(snap, &entry)) {
+		CloseHandle(snap);
+		return pids;
+	}
+	do {
+		if (wstring(entry.szExeFile) == targetProcessName) {
+			pids.emplace_back(entry.th32ProcessID);
+		}
+	} while (Process32NextW(snap, &entry));
+	CloseHandle(snap);
+	return pids;
+}
+
+BOOL CALLBACK EnumWindowsCallback(HWND hwnd, LPARAM lParam) {
+	WindowsFinderParams& params = *(WindowsFinderParams*)lParam;
+
+	unsigned char satisfiedCriteria = 0, unSatisfiedCriteria = 0;
+
+	// If looking for windows of a specific PDI
+	DWORD pid = 0;
+	GetWindowThreadProcessId(hwnd, &pid);
+	if (params.pidOwner != NULL)
+		if (params.pidOwner == pid)
+			++satisfiedCriteria; // Doesn't belong to the process targeted
+		else
+			++unSatisfiedCriteria;
+
+	// If looking for windows of a specific class
+	wchar_t className[MAX_CLASSNAME] = L"";
+	GetClassName(hwnd, className, MAX_CLASSNAME);
+	wstring classNameWstr = className;
+	if (params.wndClassName != L"")
+		if (params.wndClassName == classNameWstr)
+			++satisfiedCriteria; // Not the class targeted
+		else
+			++unSatisfiedCriteria;
+
+	// If looking for windows with a specific name
+	wchar_t windowName[MAX_WNDNAME] = L"";
+	GetWindowText(hwnd, windowName, MAX_CLASSNAME);
+	wstring windowNameWstr = windowName;
+	if (params.wndName != L"")
+		if (params.wndName == windowNameWstr)
+			++satisfiedCriteria; // Not the class targeted
+		else
+			++unSatisfiedCriteria;
+
+	// If looking for window at a specific position
+	RECT pos;
+	GetWindowRect(hwnd, &pos);
+	if (params.pos.left || params.pos.top || params.pos.right || params.pos.bottom)
+		if (params.pos.left == pos.left && params.pos.top == pos.top && params.pos.right == pos.right && params.pos.bottom == pos.bottom)
+			++satisfiedCriteria;
+		else
+			++unSatisfiedCriteria;
+
+	// If looking for window of a specific size
+	POINT res = { pos.right - pos.left, pos.bottom - pos.top };
+	if (params.res.x || params.res.y)
+		if (res.x == params.res.x && res.y == params.res.y)
+			++satisfiedCriteria;
+		else
+			++unSatisfiedCriteria;
+
+	// If looking for windows taking more than a specific percentage of all the screens
+	float ratioAllScreensX = res.x / GetSystemMetrics(SM_CXSCREEN);
+	float ratioAllScreensY = res.y / GetSystemMetrics(SM_CYSCREEN);
+	float percentAllScreens = ratioAllScreensX * ratioAllScreensY * 100;
+	if (params.percentAllScreens != 0.0f)
+		if (percentAllScreens >= params.percentAllScreens)
+			++satisfiedCriteria;
+		else
+			++unSatisfiedCriteria;
+
+	// If looking for windows taking more than a specific percentage or the main screen
+	RECT desktopRect;
+	GetWindowRect(GetDesktopWindow(), &desktopRect);
+	POINT desktopRes = { desktopRect.right - desktopRect.left, desktopRect.bottom - desktopRect.top };
+	float ratioMainScreenX = res.x / desktopRes.x;
+	float ratioMainScreenY = res.y / desktopRes.y;
+	float percentMainScreen = ratioMainScreenX * ratioMainScreenY * 100;
+	if (params.percentMainScreen != 0.0f)
+		if (percentAllScreens >= params.percentMainScreen)
+			++satisfiedCriteria;
+		else
+			++unSatisfiedCriteria;
+
+	// Looking for windows with specific styles
+	LONG_PTR style = GetWindowLongPtr(hwnd, GWL_STYLE);
+	if (params.style)
+		if (params.style & style)
+			++satisfiedCriteria;
+		else
+			++unSatisfiedCriteria;
+
+	// Looking for windows with specific extended styles
+	LONG_PTR styleEx = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+	if (params.styleEx)
+		if (params.styleEx & styleEx)
+			++satisfiedCriteria;
+		else
+			++unSatisfiedCriteria;
+
+	if (!satisfiedCriteria)
+		return TRUE;
+
+	if (params.satisfyAllCriteria && unSatisfiedCriteria)
+		return TRUE;
+
+	// If looking for multiple windows
+	params.hwnds.push_back(hwnd);
+	return TRUE;
+}
+
+vector<HWND> WindowsFinder(WindowsFinderParams params) {
+	EnumWindows(EnumWindowsCallback, (LPARAM)&params);
+	return params.hwnds;
+}
+#pragma endregion
+
 WPARAM MainLoop()
 {
 	MSG msg;
 	RECT rc;
 	
+
 	while (TRUE)
 	{
 		ZeroMemory(&msg, sizeof(MSG));
@@ -583,6 +928,8 @@ WPARAM MainLoop()
 		if (msg.message == WM_QUIT)
 			exit(0);
 		twnd = NULL;
+
+		/*
 		twnd = FindWindow(L"UnrealWindow", 0);
 		if (!twnd)
 		{
@@ -596,6 +943,8 @@ WPARAM MainLoop()
 		s_height = rc.bottom - rc.top;
 		MoveWindow(hWnd, rc.left, rc.top, s_width, s_height, true);
 		
+		*/
+
 		//render your esp
 		render();
 		
@@ -625,6 +974,11 @@ bool Attach(WCHAR* processName)
 
 int main(int argc, char* argv[])
 {
+	InitializeOverlay();
+	MakeTrasparency();
+
+	SetDCText(L"M416\nNEW", 8, 2, 200, 1000);
+
 	while (true)
 	{
 		if (Attach(L"TslGame.exe") == true)
